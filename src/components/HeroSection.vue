b04e5722a49f07a53a05d85cebe9caa4b28d8cc1
甘鸿谨 <libailinux0@gmail.com>
2026-05-09 02:25:53 +0800

    fix: further reduce hero-to-skills spacing

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -25,7 +25,7 @@ useTypewriter(subtitleRef, personalInfo.title, { duration: 1.5, delay: 0.3 })
 
 <style scoped>
 .hero {
-  min-height: 85vh;
+  min-height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
